67b3f37c75f5cfcdff0abef9ce677d797675a358
yuan <zu00423828@gmail.com>
2021-03-22 21:16:51 +0800

    video 紀錄切割時間點

---
+++ video_mark/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace video_mark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.media = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.media)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(12, 581);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "選擇影片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // media
+            // 
+            this.media.Enabled = true;
+            this.media.Location = new System.Drawing.Point(12, 50);
+            this.media.Name = "media";
+            this.media.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("media.OcxState")));
+            this.media.Size = new System.Drawing.Size(1007, 483);
+            this.media.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(203, 581);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "播放";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(400, 581);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 50);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "暫停";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(607, 581);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 50);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "繼續播放";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(805, 581);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 50);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "停止";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(1032, 375);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 256);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "紀錄分割點";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1032, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(1029, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "記錄分割點檔名\r\n以下輸入xxx.txt";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1061, 123);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "確認檔名";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1050, 197);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(107, 38);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "重置";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Visible = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 665);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.media);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.media)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer media;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ video_mark/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace video_mark
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        double time;
+        FileStream fs ;
+        StreamWriter sw;
+        FileStream fs2;
+        StreamWriter sw2;
+        string filename;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+ 
+
+
+        }
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+
+            sw.Close();
+            fs.Close();
+            sw2.Close();
+            fs2.Close();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            /*OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select file";
+            dialog.InitialDirectory = ".\\";
+            dialog.Filter = "xls files (*.*)|*.xls";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(dialog.FileName);
+            }*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opn = new OpenFileDialog();
+            opn.Filter = "(*.mp4;)|*.mp4;";
+            if (opn.ShowDialog() == DialogResult.OK)
+            {
+                //this.media.URL = opn.FileName;
+                path = opn.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.media.URL = path;
+            this.media.Ctlcontrols.play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //this.media.Ctlcontrols.pause();
+            time = this.media.Ctlcontrols.currentPosition;
+            this.media.Ctlcontrols.pause();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.media.Ctlcontrols.currentPosition = time;
+            this.media.Ctlcontrols.play();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.media.Ctlcontrols.stop();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            time = this.media.Ctlcontrols.currentPosition;
+            TimeSpan times = TimeSpan.FromSeconds(time-0.5f);
+            string str = times.ToString(@"hh\:mm\:ss\.ff");
+            //MessageBox.Show(str);
+            sw.Write(str + "\n");
+            sw.Flush();
+
+
+            TimeSpan times2 = TimeSpan.FromSeconds(time - 0.27f);
+            string str2 = times2.ToString(@"hh\:mm\:ss\.ff");
+            //MessageBox.Show(str);
+            sw2.Write(str2 + "\n");
+            sw2.Flush();
+
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            filename = textBox1.Text;
+            if (filename.IndexOf(".txt") != -1)
+            {
+                fs = new FileStream(filename, FileMode.Create);
+                sw = new StreamWriter(fs);
+                this.textBox1.Visible = false;
+                this.button7.Visible = false;
+                label1.Text = filename;
+                this.button8.Visible = true;
+
+
+                fs2 = new FileStream("new"+filename, FileMode.Create);
+                sw2 = new StreamWriter(fs2);
+
+            }
+            else MessageBox.Show("檔名不正確");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            Application.Restart();
+            
+        }
+    }
+}
